78fc625b9d6cbbc8ca737a13e7d6fc2c1fa60d56
Norberto Ortigoza <norberto.ortigoza@raku.mx>
2015-06-02 22:23:38 -0500

    Fixing bug: python processes were not created with start_link

--- lib/python_process_server/worker.ex
+++ lib/python_process_server/worker.ex
@@ -16,12 +16,14 @@ defmodule PythonProcessServer.Worker do
   # Private Functions
   ###################
   def init(python_module) do
-    {:ok, pp} = :python.start([{:python_path, to_char_list(Path.expand("lib/python_scripts"))},{:python, 'python'}])
+    IO.puts "Iniciando worker"
+    {:ok, pp} = :python.start_link([{:python_path, to_char_list(Path.expand("lib/python_scripts"))},{:python, 'python'}])
     state = {python_module, pp}
     {:ok, state}
   end
 
   def handle_call({:call_python, function, args}, _from, state) do
+    IO.puts "Procesando petición"
     {py_module, py_pid} = state
     result = :python.call(py_pid, py_module, function, args)
     reply = {:ok, result}
@@ -29,10 +31,17 @@ defmodule PythonProcessServer.Worker do
   end
 
   def handle_call(_request, _from, state) do
+    IO.puts "Procesando otros mensajes"
     {:stop, :error, :bad_call, state}
   end
 
+  def handle_info(_msg, {module,py_pid}) do
+    IO.puts "Adios"
+    {:stop, :error, {module,py_pid}}
+  end
+
   def terminate(_reason, {_, py_pid}) do
+    IO.puts "Algo salio mal"
     :python.stop(py_pid)
     :ok
   end
